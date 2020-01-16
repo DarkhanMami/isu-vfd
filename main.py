47dc01c01c8d8a6a2c8295c39d54ddc5f8f3e2d9
DarkhanMami <dmami@nu.edu.kz>
2020-01-16 16:46:05 +0600

    socket server and get states

--- main.py
+++ main.py
@@ -336,7 +336,7 @@ def start_getting_state():
             print '******************************'
             print 'start_getting_state'
             print '******************************'
-            print vfd.g120
+            print vfd.g120.get_state()
             # vfd = VFDControl()
             # for i in range(10):
             #     print '******************************'
